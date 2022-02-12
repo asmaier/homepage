9f2bd5c3f7bc14f07d1cac3628af9400bb3d16a1
asmaier <asmaier@web.de>
2022-02-12 23:34:28 +0100

    Updated version of wowchemy to 5.4.0

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:/Users/andi/NOBACKUP/github/homepage/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
+  // ns-hugo:/var/folders/sh/kwb99m2x5_v1k64g7n81g9m40000gn/T/hugo_cache/modules/filecache/modules/pkg/mod/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5@v5.0.0-20210629192904-559885af86b7/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
---
+++ go.mod
@@ -0,0 +1,7 @@
+module my-site
+
+go 1.17
+
+require (
+	github.com/wowchemy/wowchemy-hugo-modules/v5 v5.4.0
+)
